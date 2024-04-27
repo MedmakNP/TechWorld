0a76a5eea8c0defa1805d09d850687c343d77746
MedmakNP <95432399+MedmakNP@users.noreply.github.com>
2024-04-27 23:46:10 +0300

    add fix

--- src/Components/Catalog/catalog.jsx
+++ src/Components/Catalog/catalog.jsx
@@ -4,15 +4,37 @@ import "swiper/css";
 import catalogData from "../../Storage/catalogData";
 import ProductSlide from "./productSlide";
 import { useTranslation } from "react-i18next";
+import { useState, useEffect } from "react";
 
 function Catalog() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [slideCount, setSlideCount] = useState(4)
   const {t} = useTranslation()
+
+  
+  useEffect(() => {
+    const handleResize = () => {
+      const width = window.innerWidth;
+      setWindowWidth(width);
+      if (width <=1024){
+        setSlideCount(3)
+      }else{
+        setSlideCount(4)
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+
+  }, []);
   return (
     <div className={styles.catalog}>
       <div className={styles.container}>
         <h1 className={styles.title}>{t('catalog.title')}</h1>
         <div className={styles.swiperWrap}>
-          <Swiper spaceBetween={0} slidesPerView={4}>
+          <Swiper spaceBetween={0} slidesPerView={slideCount}>
             {catalogData.map((val) => (
               <SwiperSlide>
                 <ProductSlide data={val} />
--- src/Components/CategoryShop/categoryShop.jsx
+++ src/Components/CategoryShop/categoryShop.jsx
@@ -3,17 +3,39 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import categoryData from "../../Storage/categoryData";
 import { ThemeContext } from "../../Providers/ThemeProvider";
-import { useContext } from "react";
+import { useContext, useState, useEffect} from "react";
 import { useTranslation } from "react-i18next";
 
 function CategoryShop() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [slideCount, setSlideCount] = useState(3)
   const { type } = useContext(ThemeContext);
   const {t} = useTranslation()
+
+  
+  useEffect(() => {
+    const handleResize = () => {
+      const width = window.innerWidth;
+      setWindowWidth(width);
+      if (width <=768){
+        setSlideCount(2)
+      }else{
+        setSlideCount(3)
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+
+  }, []);
+  console.log(windowWidth ,slideCount)
   return (
     <div className={styles.categoryShop}>
       <div className={styles.container}>
         <h1 className={styles.title}>{t('category.title')}</h1>
-        <Swiper spaceBetween={30} slidesPerView={3}>
+        <Swiper spaceBetween={30} slidesPerView={slideCount}>
           {categoryData.map((val) => (
             <SwiperSlide>
               <div className={`${styles.slide} ${styles[type]}`}>
